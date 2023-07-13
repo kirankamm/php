76bc95f45dea228a258555b3931352b120fd7c78
kirankamm <118911532+kirankamm@users.noreply.github.com>
2023-07-13 22:51:33 +0530

    Add files via upload

---
+++ variable.php
@@ -0,0 +1,10 @@
+<?php
+
+$name='Raju his from Bengluru';
+$age=25;
+
+$paragrap='Lorem Ipsum '.$name.'  is simply dummy text of the printing and typesetting industry';
+
+echo $name .'  '. $age;
+echo $paragrap;
+?>
