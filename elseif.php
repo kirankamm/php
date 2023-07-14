3750c5c4cecf6c84432aff20a140802dae77b623
kirankamm <118911532+kirankamm@users.noreply.github.com>
2023-07-14 18:42:49 +0530

    Add files via upload

---
+++ elseif.php
@@ -0,0 +1,17 @@
+<?php
+
+$amount=300;
+
+if($amount==100){
+    
+    echo'1 pen get';
+}else if($amount>=200 AND $amount>=199)  {
+    echo'get 2 pen';
+    
+}else if($amount>=300 AND $amount<299)  {
+    echo'get 3 pen';
+    
+} 
+
+
+?>
---
+++ if-else-elseif.php
@@ -0,0 +1,16 @@
+<?php
+
+$amount=100;
+
+if($amount=100){
+    
+    echo'1 pen get';
+}else  {
+    echo'not get';
+    
+}
+
+
+
+
+?>
