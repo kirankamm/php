dd4660b33b9c8f424cea8f0fb4a10f4169f75717
kirankamm <118911532+kirankamm@users.noreply.github.com>
2023-07-14 12:32:16 +0530

    Add files via upload

---
+++ colorpadding.php
@@ -0,0 +1,11 @@
+<?php
+
+
+
+$name='Bengluru';
+$variable='<div style="background:DarkMagenta; color:white; padding:5px;"><h1>Hi karnataka</h1></div>';
+
+echo $name;
+echo $variable;
+
+?>
---
+++ emailpassword.php
@@ -0,0 +1,11 @@
+<?php
+
+$Email='RajuPatil@gmail';
+$password=2525;
+
+$text='Lorem Ipsum '.$Email.'  is simply dummy text of the printing and typesetting industry';
+
+echo $Email .' ------ '. $password;
+	
+echo $text;
+?>
